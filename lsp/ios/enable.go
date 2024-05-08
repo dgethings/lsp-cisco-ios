3c79ec7e1b2a4ad2fee2a61582a4b8bc966ef47b
David Gethings <dgethings@me.com>
2024-05-08 23:13:39 +0100

    refactor: a different attempt

--- lsp/ios/enable.go
+++ lsp/ios/enable.go
@@ -1,5 +1,48 @@
 package ios
 
-var Enable = map[string]string{
-	"enable": "password",
+import protocol "github.com/tliron/glsp/protocol_3_16"
+
+type Keyword struct {
+	Keyword       string
+	Documentation string
+	Insert        string
+}
+
+func NewKeyword(k string, d string, i string) Keyword {
+	return Keyword{Keyword: k, Documentation: d, Insert: i}
+}
+
+func (k *Keyword) Completion() protocol.CompletionItem {
+	kind := protocol.CompletionItemKindKeyword
+	format := protocol.InsertTextFormatSnippet
+	c := protocol.CompletionItem{
+		Label: k.Keyword,
+		Documentation: protocol.MarkupContent{
+			Kind:  protocol.MarkupKindMarkdown,
+			Value: k.Documentation,
+		},
+	}
+	c.InsertText = &k.Insert
+	c.Kind = &kind
+	c.InsertTextFormat = &format
+	return c
+}
+
+var (
+	EnablePassword = NewKeyword("enable password", "To set a local password to control access to various privilege levels, use the enable password command in global configuration mode. To remove the password requirement, use the no form of this command.\n\nenable password _level_ {password | _encryption-type_ encrypted-password}\nno enable password _level_\n", "enable password ${1:15} ${2} ${3:5} ${4}")
+	EnableSecret   = NewKeyword("enable secret", "To specify an additional layer of security over the enable password command, use the enable secret command in global configuration mode. To turn off the enable secret function, use the no form of this command.\n\nenable secret _level_ { 0 _unencrypted-password_ | _encryption-type_ _encrypted-password_}\nno enable secret _level_ _encryption-type encrypted-password_", "enable secret ${1:15} ${2:0} ${3}")
+)
+
+func Completions() []protocol.CompletionItem {
+	return []protocol.CompletionItem{
+		EnablePassword.Completion(),
+		EnableSecret.Completion(),
+	}
+}
+
+func Keywords() map[string]Keyword {
+	k := make(map[string]Keyword)
+	k[EnablePassword.Keyword] = EnablePassword
+	k[EnableSecret.Keyword] = EnableSecret
+	return k
 }
--- lsp/textdocument/hover.go
+++ lsp/textdocument/hover.go
@@ -5,6 +5,7 @@ import (
 	"log/slog"
 	"strings"
 
+	"github.com/dgethings/lsp-cisco-ios/lsp/ios"
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 )
@@ -19,25 +20,49 @@ func Hover(ctx *glsp.Context, params *protocol.HoverParams) (*protocol.Hover, er
 }
 
 // Finds the word surrounding the given line and char number
-func selectedWord(f string, l uint, c uint) string {
-	line := strings.Split(f, "\n")
-	word := line[l]
+func selectedWord(contents string, lineNum uint, colNum uint) string {
+	slog.Info("in the func")
+	line := strings.Split(contents, "\n")[lineNum]
+	word := line
+	slog.Info("setting default match", "word", word)
 	// if the char is a space then return empty string
-	if string(line[l][c]) == " " {
+	if string(line[colNum]) == " " {
 		return ""
 	}
 	// find the " " closest to the char index, that should be the start of the word
-	start := strings.LastIndex(line[l][:c], " ")
+	start := strings.LastIndex(line[:colNum], " ")
 	slog.Info("start", "index", start)
-	end := strings.Index(line[l][c+1:], " ")
-	slog.Info("end", "end", int(c)+end+1)
+	end := strings.Index(line[colNum+1:], " ")
+	slog.Info("end", "end", int(colNum)+end+1)
 	if start > 0 && end > 0 {
-		word = line[l][start : int(c)+end+1]
+		word = line[start : int(colNum)+end+1]
 	} else if start == -1 && end > 0 {
-		word = line[l][0 : int(c)+end+1]
+		word = line[0 : int(colNum)+end+1]
 	} else if start > 0 && end == -1 {
-		word = line[l][start:]
+		word = line[start:]
 	}
 	slog.Info("found word", "word", word)
 	return word
+}
+
+// return the matching keyword
+func getKeyword(contents string, lineNum uint, colNum uint) ios.Keyword {
+	word := getSelectedWord(contents, lineNum, colNum)
+	var keywords []ios.Keyword
+	for _, kw := range ios.Keywords() {
+		if strings.Contains(kw.Keyword, word) {
+			keywords = append(keywords, kw)
+		}
+	}
+	if len(keywords) == 1 {
+		return keywords[0]
+	} else {
+		return keywords[0]
+		// return uniqueKeyword()
+	}
+}
+
+// from the given file, line number and column number, find the surrounding word
+func getSelectedWord(contents string, lineNum uint, colNum uint) string {
+	return "foo"
 }
