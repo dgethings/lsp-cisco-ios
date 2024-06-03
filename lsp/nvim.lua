f01983e9c41d6374540a8fa1432770b58c40b592
David Gethings <dgethings@me.com>
2024-06-03 23:44:01 +0100

    feat(lsp): example LSP config

--- lsp/nvim.lua
+++ lsp/nvim.lua
@@ -1,3 +1,6 @@
+vim.filetype.add({
+	pattern = { ["ios.*j2"] = "IOS-JINJA" },
+})
 local client = vim.lsp.start_client({
 	cmd = { "/Users/dgethings/git/lsp-cisco-ios/lsp/ios-lsp" },
 	name = "iosLSP",
@@ -10,7 +13,7 @@ end
 vim.notify("Starting IOS LSP " .. client)
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "conf",
+	pattern = "IOS-JINJA",
 	callback = function()
 		vim.lsp.buf_attach_client(0, client)
 		local suc = vim.lsp.buf_is_attached(0, client)
